315e2c07cac3027e7a8e72c019c080a3085435b5
akr7777 <dimitrius77@protonmail.com>
2024-04-09 02:36:35 +0300

    one video added

--- src/chapters/chapter4_videos/chapter4-videos.tsx
+++ src/chapters/chapter4_videos/chapter4-videos.tsx
@@ -9,19 +9,22 @@ export const ChapterFour_Videos = () => {
             <div className="iframe_div">
                 <iframe src="https://www.youtube.com/embed/QR0fg1xqc7A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
+            <div className="iframe_div">
+                <iframe src="https://www.youtube.com/embed/lir2HguvXq0?si=p_J3ay3a4kxd9uIU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            </div>
             <div className="others_video_div">
-            <div className="centrDiv">
-                <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/lo8kj71nSi0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            </div>
-            <div className="centrDiv">
-                <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/IgdjYRbpZto" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            </div>
-            <div className="centrDiv">
-                <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/l7sHfd1C4ys" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            </div>
-            <div className="centrDiv">
-                <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/GypWh72MQxc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            </div>
+                <div className="centrDiv">
+                    <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/lo8kj71nSi0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </div>
+                <div className="centrDiv">
+                    <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/IgdjYRbpZto" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </div>
+                <div className="centrDiv">
+                    <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/l7sHfd1C4ys" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </div>
+                <div className="centrDiv">
+                    <iframe className="one_other_video_div" src="https://www.youtube-nocookie.com/embed/GypWh72MQxc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </div>
             </div>
 
         </Chapter>
